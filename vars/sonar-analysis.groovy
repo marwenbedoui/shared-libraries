81b3263601513d73fe4fcc7c10e88271f6bd686c
marwenbedoui <66925576+marwenbedoui@users.noreply.github.com>
2024-12-02 09:07:16 +0100

    Delete vars/sonar-analysis.groovy

--- vars/sonar-analysis.groovy
+++
@@ -1,7 +0,0 @@
-
-// File: vars/sonarAnalysis.groovy
-
-def call() {
-   def SCANNER_HOME= tool 'sonar-scanner'
-   sh ' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=E-Kart  -Dsonar.java.binaries=.  -Dsonar.projectKey=E-Kart '''
-}
